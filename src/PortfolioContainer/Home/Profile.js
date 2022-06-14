475a68df888907d11c5fbe5ff1c181d129893816
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2022-06-15 03:31:07 +0530

    PortFolio App Using React

---
+++ src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Profile() {
+  return (
+    <div className='profile-container'>
+        <div className='profile-parent'>
+            <div className='profile-details'>
+                <div className='colz'>
+                    Vaibhav Gawad On Your Service Sir
+                                    
+                    <i class="fa-solid fa-user"></i>
+                    <i class="fa-regular fa-user"></i>
+
+                    <i class="fa-light fa-user"></i>
+
+                    <i class="fa-duotone fa-user"></i>
+                    <i class="fa-thin fa-user"></i>
+
+                    <i class="fa-brands fa-github-square"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
